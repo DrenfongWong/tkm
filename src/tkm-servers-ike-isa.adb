cbc3be7a94481c668c676cbfd807337606537d88
Reto Buerki <reet@codelabs.ch>
2012-08-24 17:17:36 +0200

    Calculate SKEYSEED in Ike.Isa.Create procedure

--- src/tkm-servers-ike-isa.adb
+++ src/tkm-servers-ike-isa.adb
@@ -1,4 +1,9 @@
+with Tkmrpc.Contexts.Dh;
+with Tkmrpc.Contexts.Nc;
+
+with Tkm.Utils;
 with Tkm.Logger;
+with Tkm.Crypto.Hmac_Sha512;
 
 package body Tkm.Servers.Ike.Isa
 is
@@ -22,11 +27,58 @@ is
       Sk_Ei     : out Tkmrpc.Types.Key_Type;
       Sk_Er     : out Tkmrpc.Types.Key_Type)
    is
-      pragma Unreferenced (Ae_Id, Ia_Id, Nonce_Rem, Initiator, Spi_Loc,
-                           Spi_Rem, Sk_Ai, Sk_Ar, Sk_Ei, Sk_Er, Dh_Id,
-                           Nc_Loc_Id);
+      pragma Unreferenced (Ae_Id, Ia_Id, Sk_Ai, Sk_Ar, Sk_Ei, Sk_Er);
+
+      Secret    : Tkmrpc.Types.Dh_Key_Type;
+      Nonce_Loc : Tkmrpc.Types.Nonce_Type;
    begin
-      L.Log (Message => "Creating new ISA context (ID" & Isa_Id'Img & ")");
+      L.Log (Message => "Creating new ISA context with ID" & Isa_Id'Img
+             & " (DH" & Dh_Id'Img & ", nonce" & Nc_Loc_Id'Img & ", spi_loc"
+             & Spi_Loc'Img & ", spi_rem" & Spi_Rem'Img & ")");
+
+      --  TODO: Use DH consume here, but this is not yet possible because
+      --        charon does key derivation twice at the moment (using the
+      --        ikev2 keymat proxy).
+
+      Secret := Tkmrpc.Contexts.Dh.Get_Shared_Secret (Id => Dh_Id);
+--        Tkmrpc.Contexts.Dh.Consume (Id     => Dh_Id,
+--                                    Dh_Key => Secret);
+--        L.Log (Message => "DH context" & Dh_Id'Img & " consumed");
+
+      Tkmrpc.Contexts.Nc.Consume (Id    => Nc_Loc_Id,
+                                  Nonce => Nonce_Loc);
+      L.Log (Message => "Nonce context" & Nc_Loc_Id'Img & " consumed");
+
+      --  Use PRF-HMAC-SHA512 for now.
+
+      declare
+         use type Tkmrpc.Types.Init_Type;
+
+         Prf         : Crypto.Hmac_Sha512.Context_Type;
+         Skeyseed    : Tkmrpc.Types.Byte_Sequence
+           (1 .. Crypto.Hmac_Sha512.Hash_Output_Length);
+         Fixed_Nonce : Tkmrpc.Types.Byte_Sequence
+           (1 .. Nonce_Rem.Size + Nonce_Loc.Size);
+      begin
+         if Initiator = 1 then
+            Fixed_Nonce (Fixed_Nonce'First .. Nonce_Loc.Size)
+              := Nonce_Loc.Data (Nonce_Loc.Data'First .. Nonce_Loc.Size);
+            Fixed_Nonce (Nonce_Loc.Size + 1 .. Fixed_Nonce'Last)
+              := Nonce_Rem.Data (Nonce_Rem.Data'First .. Nonce_Rem.Size);
+         else
+            Fixed_Nonce (Fixed_Nonce'First .. Nonce_Rem.Size)
+              := Nonce_Rem.Data (Nonce_Rem.Data'First .. Nonce_Rem.Size);
+            Fixed_Nonce (Nonce_Rem.Size + 1 .. Fixed_Nonce'Last)
+              := Nonce_Loc.Data (Nonce_Loc.Data'First .. Nonce_Loc.Size);
+         end if;
+
+         Crypto.Hmac_Sha512.Init (Ctx => Prf,
+                                  Key => Fixed_Nonce);
+         Skeyseed := Crypto.Hmac_Sha512.Generate (Ctx  => Prf,
+                                                  Data => Secret.Data);
+         L.Log (Message => "SKEYSEED " & Utils.To_Hex_String
+                (Input => Skeyseed));
+      end;
    end Create;
 
 end Tkm.Servers.Ike.Isa;
