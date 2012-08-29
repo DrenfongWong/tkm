3f1e56577d87babbdfd3b9d5c054380fc2f0d690
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2012-08-29 17:55:57 +0200

    Minor: Fix indentation

--- src/tkm-servers-ike-isa.adb
+++ src/tkm-servers-ike-isa.adb
@@ -17,15 +17,15 @@ is
    -------------------------------------------------------------------------
 
    procedure Create
-     (Isa_Id    : Tkmrpc.Types.Isa_Id_Type;
-      Ae_Id     : Tkmrpc.Types.Ae_Id_Type;
-      Ia_Id     : Tkmrpc.Types.Ia_Id_Type;
-      Dh_Id     : Tkmrpc.Types.Dh_Id_Type;
-      Nc_Loc_Id : Tkmrpc.Types.Nc_Id_Type;
-      Nonce_Rem : Tkmrpc.Types.Nonce_Type;
-      Initiator : Tkmrpc.Types.Init_Type;
-      Spi_Loc   : Tkmrpc.Types.Ike_Spi_Type;
-      Spi_Rem   : Tkmrpc.Types.Ike_Spi_Type;
+     (Isa_Id    :     Tkmrpc.Types.Isa_Id_Type;
+      Ae_Id     :     Tkmrpc.Types.Ae_Id_Type;
+      Ia_Id     :     Tkmrpc.Types.Ia_Id_Type;
+      Dh_Id     :     Tkmrpc.Types.Dh_Id_Type;
+      Nc_Loc_Id :     Tkmrpc.Types.Nc_Id_Type;
+      Nonce_Rem :     Tkmrpc.Types.Nonce_Type;
+      Initiator :     Tkmrpc.Types.Init_Type;
+      Spi_Loc   :     Tkmrpc.Types.Ike_Spi_Type;
+      Spi_Rem   :     Tkmrpc.Types.Ike_Spi_Type;
       Sk_Ai     : out Tkmrpc.Types.Key_Type;
       Sk_Ar     : out Tkmrpc.Types.Key_Type;
       Sk_Ei     : out Tkmrpc.Types.Key_Type;
