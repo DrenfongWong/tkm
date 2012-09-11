4f864c97bd0ba6cb08dc5f279b720ce7f5c5832d
Reto Buerki <reet@codelabs.ch>
2012-09-11 17:43:43 +0200

    Add ESP SA test configuration

--- src/tkm-config.ads
+++ src/tkm-config.ads
@@ -4,4 +4,13 @@ is
    Pre_Shared_Key : constant String := "foobar";
    --  Pre-shared key used for (PSK) authentification.
 
+   Local_Addr : constant String := "152.96.15.32";
+   --  ESP source address.
+
+   Peer_Addr : constant String := "152.96.15.60";
+   --  ESP destination address.
+
+   Lifetime : constant := 3600;
+   --  ESP SA lifetime in seconds.
+
 end Tkm.Config;
