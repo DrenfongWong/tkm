433842d3b3c2a17df1796f2e28ab85d9a8c41274
Reto Buerki <reet@codelabs.ch>
2012-10-31 13:38:55 +0100

    Add Sha256_Digest_Info constant

--- src/tkm-digests.ads
+++ src/tkm-digests.ads
@@ -1,7 +1,12 @@
 package Tkm.Digests
 is
 
-   Sha1_Digest_Info : constant String := "3021300906052b0e03021a05000414";
+   Sha1_Digest_Info   : constant String
+     := "3021300906052b0e03021a05000414";
    --  DER encoding T of the DigestInfo value for SHA-1.
+
+   Sha256_Digest_Info : constant String
+     := "3031300d060960864801650304020105000420";
+   --  DER encoding T of the DigestInfo value for SHA-256.
 
 end Tkm.Digests;
